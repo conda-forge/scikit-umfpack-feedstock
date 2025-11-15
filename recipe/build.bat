25cfef4079e6ce1cf6e65c174ff537e63e728aba
Rich Fiorella <rich.p.fiorella@gmail.com>
2025-11-15 11:18:06 -0700

    wip - attempt to debug windows install that is now  broken

--- recipe/build.bat
+++ recipe/build.bat
@@ -1,10 +1,55 @@
-@echo on
-setlocal enabledelayedexpansion
+@echo off
+setlocal
 
-echo [properties]> nativefile.ini
-echo umfpack-libdir = '%PREFIX%\Library\lib'>> nativefile.ini
-echo umfpack-includedir = '%PREFIX%\Library\include\suitesparse'>> nativefile.ini
+REM Debug the env just so you can see it in Azure logs
+echo SRC_DIR=%SRC_DIR%
+echo PREFIX=%PREFIX%
+echo LIBRARY_LIB=%LIBRARY_LIB%
+echo LIBRARY_INC=%LIBRARY_INC%
 
-REM Now build with pip, passing the Meson native file via config-settings
-"%PYTHON%" -m pip install . --no-deps -vv -Csetup-args=--native-file=%CD%\nativefile.ini
+echo ===== DIR LIBRARY_LIB =====
+dir "%LIBRARY_LIB%"
+echo ===== DIR LIBRARY_INC\suitesparse =====
+dir "%LIBRARY_INC%\suitesparse"
 
+REM Create Meson native file
+set "NATIVE_FILE=%SRC_DIR%\nativefile.ini"
+if exist "%NATIVE_FILE%" del "%NATIVE_FILE%"
+
+echo [properties]> "%NATIVE_FILE%"
+
+REM Escape backslashes for Meson
+set "UMF_LIBDIR=%LIBRARY_LIB%"
+set "UMF_LIBDIR=%UMF_LIBDIR:\=\\%"
+echo umfpack-libdir = '%UMF_LIBDIR%'>> "%NATIVE_FILE%"
+
+set "UMF_INCDIR=%LIBRARY_INC%\suitesparse"
+set "UMF_INCDIR=%UMF_INCDIR:\=\\%"
+echo umfpack-includedir = '%UMF_INCDIR%'>> "%NATIVE_FILE%"
+
+echo ===== nativefile.ini =====
+type "%NATIVE_FILE%"
+echo ==========================
+
+REM Build & install using pip/meson
+"%PYTHON%" -m pip install . -vv ^
+  --no-build-isolation ^
+  --config-settings=setup-args="--native-file=%NATIVE_FILE%"
+
+if errorlevel 1 (
+  echo Pip/Meson build failed
+  exit 1
+)
+
+REM Quick smoke-test *inside build env* to make sure API is present
+"%PYTHON%" -c "import scikits.umfpack as um; \
+print('has spsolve:', hasattr(um, 'spsolve')); \
+print('has splu:', hasattr(um, 'splu')); \
+print('has UmfpackContext:', hasattr(um, 'UmfpackContext'))"
+
+if errorlevel 1 (
+  echo scikits.umfpack smoke-test failed
+  exit 1
+)
+
+endlocal
