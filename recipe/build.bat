a914a44b067805581a830272479f60f91b2f0fce
Rich Fiorella <rich.p.fiorella@gmail.com>
2025-11-14 23:02:06 -0700

    simplify windows build

--- recipe/build.bat
+++ recipe/build.bat
@@ -1,13 +1,6 @@
 @echo on
 setlocal enabledelayedexpansion
 
-REM Convert MESON_ARGS="--foo bar" to "-Csetup-args=--foo -Csetup-args=bar"
-set "MESON_SETUP_ARGS="
-if not "%MESON_ARGS%"=="" (
-    for %%A in (%MESON_ARGS%) do (
-        set "MESON_SETUP_ARGS=!MESON_SETUP_ARGS! -Csetup-args=%%A"
-    )
-)
 
-"%PYTHON%" -m pip install . --no-deps -vv %MESON_SETUP_ARGS%
+"%PYTHON%" -m pip install . --no-build-isolation --no-deps -vv
 
